17117305aa08e4ceb018a3905485347447191c12
unknown <chtae0314@naver.com>
2023-01-09 15:42:04 +0900

    파이보 프로젝트 최초 커밋

--- config/production.py
+++ config/production.py
@@ -1,5 +1,7 @@
 from logging.config import dictConfig
 
+from config.default import *
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'D=\xa9\xc2\xac2\x98y\xa3\xd6ar#\xbc\xf4b'
